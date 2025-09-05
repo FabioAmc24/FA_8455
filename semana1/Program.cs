4f323d18ac554d6581352d85db9b3487220c2baa
catherine <catherinerv.suga@gmail.com>
2025-09-05 08:18:44 -0500

    subiendo ejer1 de la semanal

---
+++ semana1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+            Console.ReadKey();  
+
+
+
+        }
+
+        static void ejer1()
+        {
+            string nombre, carrera;
+            Console.Write("Ingresa su nomnbre: ");
+            nombre= Console.ReadLine();
+            Console.Write("INgrese su carrera: ");
+            carrera= Console.ReadLine();
+
+            Console.WriteLine($"\n {nombre} bienvenido al curso de fundamentos algoritmicos de {carrera}");
+
+        }
+
+    }
+}
---
+++ semana1py/semana1py.py
@@ -0,0 +1,9 @@
+from unicodedata import bidirectional
+
+
+def ejer1():
+    nombre = input("Ingrese su nombre: ")
+    carrera= input("Ingresar su carrera: ")
+    print(f"\n{nombre}, bienvenido al curso de FA de {carrera}")
+
+ ejer1()
